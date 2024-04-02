de3383dee2864c162249984b6f75d6392a33e312
itszackbabyy <zakyx1ggg@gmail.com>
2024-04-02 14:18:54 +0800

    commit pertama

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+include_once("inc/conn.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+        }
+
+        .container {
+            max-width: 400px;
+            margin: 50px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgb(0, 0, 0, 0, 1);
+        }
+    </style>
+</head>
+
+<body>
+    <div>
+    <form method="POST">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit" name="login">Login</button>
+        <a href="register.php">Belum Punya Akun</a>
+    </form>
+    </div>
+</body>
+
+</html>
